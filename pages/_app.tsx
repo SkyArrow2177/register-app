4a14b14ffec7b1ccd673fedada8b8572ca717672
SkyArrow2177 <ianchen7777@gmail.com>
2022-08-02 11:47:04 +1000

    custom app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,41 @@
-import 'styles/globals.css';
-
+import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
+import type { ColorScheme } from '@mantine/styles';
+import { QueryClient } from '@tanstack/query-core';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { setCookies } from 'cookies-next';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+type CustomAppProps = AppProps & {
+  initialColorScheme: ColorScheme;
+};
+
+const queryClient = new QueryClient();
+
+export default function App({ Component, pageProps, initialColorScheme }: CustomAppProps) {
+  // Dark mode support
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(initialColorScheme);
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
+    setColorScheme(nextColorScheme);
+    setCookies('mantine-color-scheme', nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <NotificationsProvider>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </NotificationsProvider>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
 }
-
-export default MyApp;
